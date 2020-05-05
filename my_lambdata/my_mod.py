2dc8f17718053c9e45aee2dfcc4b1bfeb9edd6da
jdblackstar <josh@blackstar.dev>
2020-05-05 10:41:11 -0700

    trimmed tailing whitespace

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,7 +1,7 @@
 def tvt_split(df):
     # don't need these, right?
-    import pandas as pd 
-    import numpy as np 
+    import pandas as pd
+    import numpy as np
     from sklearn.model_selection import train_test_split
 
     train, test = train_test_split(df, test_size=0.1)
@@ -11,9 +11,9 @@ def tvt_split(df):
 
 def only_nulls(df):
     # don't need these right?
-    import pandas as pd 
-    import numpy as np 
-    
+    import pandas as pd
+    import numpy as np
+
     dfx = df[df.isnull().any(axis=1)]
 
     return dfx
