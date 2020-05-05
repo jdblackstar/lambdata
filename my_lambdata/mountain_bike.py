918ea42a33dad1752697439bd0f0209dd0c38214
jdblackstar <josh@blackstar.dev>
2020-05-05 16:22:08 -0700

    mountainbike class for code review

---
+++ my_lambdata/mountain_bike.py
@@ -0,0 +1,60 @@
+class MountainBike():
+    """
+    MountainBike class
+
+    Attributes:
+    - wheel_size: int (27.5, 29)
+    - suspension: str ('full', 'soft', 'hard')
+    - brand: str ('Specialized', 'Trek', 'Yeti')
+
+    Methods:
+    - wheelie (property)
+    - jump (property)
+    - crash (static)
+
+    """
+
+    def __init__(self, wheel_size, suspension, brand):
+        self.wheel_size = wheel_size
+        self.suspension = suspension
+        self.brand = brand
+
+    @property
+    def wheelie(self):
+        if self.wheel = 27:
+            print(f'Bikes with {self.wheel} in wheels were made for wheelies!')
+        else if self.wheel = 29:
+            print(f"If you can wheelie on a {self.wheel}er, you're a God")
+        else:
+            return
+            
+    @property
+    def jump(self):
+        if self.suspension = "full":
+            print(f'Jumps are easy for a bike with {self.suspension} suspension!')
+        else if self.suspension = 'soft':
+            print(f'This could hurt a little bit...')
+        else if self.suspension = 'hard':
+            print(f"Don't even try it, brother.")
+        else:
+            return
+
+
+    @staticmethod
+    def crash():
+        print(f'Ouch...')
+
+
+if __name__ == "__main__":
+
+    bike1 = MountainBike(wheel_size=27.5, suspension='soft', brand='Trek')
+    print(bike1.wheel_size, bike1.suspension, bike1.brand)
+    bike1.wheelie
+
+    bike2 = MountainBike(wheel_size=29, suspension='full', brand='Specialized')
+    print(bike2.wheel_size, bike2.suspension, bike2.brand)
+    bike2.jump
+
+    bike3 = MountainBike(wheel_size=29, suspension='hard', brand='Yeti')
+    print(bike3.wheel_size, bike3.suspension, bike3.brand)
+    bike3.crash
