bbd4767673399e26e8bf3d2403eb2d0e6ec60480
jdblackstar <josh@blackstar.dev>
2020-05-04 17:51:14 -0700

    modify my_mod

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -1,10 +1,20 @@
+def tvt_split(df):
+    import pandas as pd 
+    import numpy as np 
+    from sklearn.model_selection import train_test_split
 
+    train, test = train_test_split(df, test_size=0.1)
+    train, val = train_test_split(train, test_size=0.1)
+
+    return train, val, test
+
+def only_nulls(df):
+    import pandas as pd 
+    import numpy as np 
+    
+    dfx = df[df.isnull().any(axis=1)]
+
+    return dfx
 
 if __name__ == "__main__":
-    pass
-
-def large():
-    return 0
-
-def small():
-    return 0
+    print("Don't run this file.")
